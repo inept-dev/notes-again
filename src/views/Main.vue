5a221429036e72c89809aba9fd81eaadb0047ba5
inept-dev <63522725+inept-dev@users.noreply.github.com>
2020-09-08 18:18:59 -0500

    base structure in place

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home">
+		<h1>MAIN</h1>
+		<img alt="Vue logo" src="../assets/logo.png" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Main",
+	components: {},
+};
+</script>
+
+<style scoped>
+</style>
